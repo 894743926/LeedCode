f1e938d27ae8415b8d3c99fd653fde466ff92aae
894743926 <894743926@qq.com>
2020-02-25 20:39:24 +0800

    my CProgram

---
+++ a.cpp
@@ -0,0 +1,22 @@
+/*************************************************************************
+	> File Name: a.cpp
+	> Author: 
+	> Mail: 
+	> Created Time: 2020年02月25日 星期二 20时25分03秒
+ ************************************************************************/
+
+#include<iostream>
+#include<cstdlib>
+#include<cstdio>
+#include<cstring>
+#include<ctime>
+#include<algorithm>
+#include<vector>
+#include<map>
+#include<cmath>
+#include<queue>
+using namespace std;
+
+int main() {
+    cout << "hello " << endl;
+}
